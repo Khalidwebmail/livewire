7643acadd71da685e8f44e1fb33fc35bc6ade2d5
Ahmed Abdullah Khaled <csewebmail@gmail.com>
2020-12-16 13:19:51 +0600

    store comment to db

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -10,13 +10,7 @@ class Comments extends Component
 {
     public $newComment;
 
-    public $comments = [
-        [
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui porro animi molestias minus ad sunt velit ipsa, iure earum vitae veritatis, culpa nihil molestiae et repellendus! Dolore dicta eius voluptates!',
-            'created_at' => '3 min ago',
-            'creator' => 'Khalid Ahmed'
-        ]
-    ];
+    public $comments;
 
     /**
      * addComment
@@ -29,17 +23,21 @@ class Comments extends Component
         {
             return;
         }
-        array_unshift($this->comments, [
-            'body' => $this->newComment,
-            'created_at' => Carbon::now()->diffForHumans(),
-            'creator' => 'Noor Hossain'
+
+        $createdComment = Comment::create([
+            'body'=>$this->newComment,
+            'user_id'=> 1
         ]);
+
+        // $this->comments->push($createdComment);
+
+        $this->comments->prepend($createdComment);
         $this->newComment = "";
     }
 
     public function mount()
     {
-        $initialComments = Comment::all();
+        $initialComments = Comment::latest()->get();
         $this->comments = $initialComments;
     }
 
